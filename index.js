59cd03afeeeffc492d52f1bd053ef637098a05d6
mattorth <orth.mj@gmail.com>
2020-01-26 08:04:18 -0800

    heroku bug fix

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const server = require('./server');
 require('dotenv').config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 // listen on port 4000
-server.listen(4000, () => {
+server.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
